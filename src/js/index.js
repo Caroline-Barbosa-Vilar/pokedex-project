f436a7f22ce54c6af7f26c1c55069eeb973aa50e
Caroline Barbosa <bo.varig@gmail.com>
2022-03-13 22:28:39 -0300

    create: the js code

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const pokemonSelectionList = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.pokemon-card')
+
+pokemonSelectionList.forEach(pokemon => {
+  
+  pokemon.addEventListener('click', () => {
+    const openedPokemonCard = document.querySelector('.open')
+    openedPokemonCard.classList.remove('open')
+
+    const idSelectedPokemon = pokemon.attributes.id.value
+
+    const idPokemonCardToOpen = 'card-' + idSelectedPokemon
+    const openPokemonCard = document.getElementById(idPokemonCardToOpen)
+    openPokemonCard.classList.add('open')
+
+    const activePokemonList = document.querySelector('.active')
+    activePokemonList.classList.remove('active')
+
+    const selectPokemon = document.getElementById(idSelectedPokemon)
+    selectPokemon.classList.add('active')
+  })
+})
